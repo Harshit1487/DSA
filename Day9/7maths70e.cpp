a774a2cdb3a61227f1424d62619d369840b8b534
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2022-05-18 17:49:59 +0530

    q7

---
+++ Day9/7maths70e.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res=nums.size();
+        
+        for(int i=0;i<nums.size();i++){
+            res ^=i;
+            res ^=nums[i];  
+        }
+        
+        return res;
+    }
+};
+
+//https://leetcode.com/problems/missing-number/
