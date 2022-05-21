ac4211b65038e5d82735eb2016e35ffc1f19e1ff
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2022-05-21 17:40:38 +0530

    q4

---
+++ Day 11/4s&s90h.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0,r=n-1;
+        
+        while(l<=r){
+            int m=(l+r)/2;
+            
+            if(nums[m]==target) return m;
+            
+            else if(nums[m]>=nums[l]){
+                if(target>=nums[l] && target<=nums[m])
+                    r=m-1;
+                else l=m+1;
+                
+            }
+            
+            else{
+                if(target<=nums[r] && target>=nums[m])
+                    l=m+1;
+                else r=m-1;
+            
+            }
+        }
+        
+        return -1;
+    }
+};
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
