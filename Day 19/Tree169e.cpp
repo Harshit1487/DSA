1700b449682aa3500b5562c58876081b812bf22d
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2022-05-30 18:13:11 +0530

    8q169

---
+++ Day 19/Tree169e.cpp
@@ -0,0 +1,13 @@
+//https://practice.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1
+
+
+bool solve(Node* root, int min, int max){
+    if(!root) return false;
+    if(min==max) return true;
+    return solve(root->left, min, root->data-1) or solve(root->right, root->data+1, max);
+}
+
+bool isDeadEnd(Node *root)
+{
+    return solve(root, 1, INT_MAX);
+}
