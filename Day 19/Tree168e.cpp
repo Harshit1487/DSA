141fe0e95a6e1ff74c1352c211915d311dffa572
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2022-05-30 18:11:30 +0530

    7q168

---
+++ Day 19/Tree168e.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& a){
+        if(!root) return;
+        inorder(root->left, a);
+        a.push_back(root->val);
+        inorder(root->right, a);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> a;
+        inorder(root, a);
+        return a;
+    }
+};
